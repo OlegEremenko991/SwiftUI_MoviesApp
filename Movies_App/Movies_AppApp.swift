6b2dd3c3930ecf54655730b471b6b5b98c71f275
Oleg991 <o.n.eremenko@gmail.com>
2021-03-01 10:15:17 +0400

    Initial Commit

---
+++ Movies_App/Movies_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_AppApp.swift
+//  Movies_App
+//
+//  Created by Олег Еременко on 01.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Movies_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
