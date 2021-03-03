4f36180be8c4a3c5ee0fbc43d2f732bc6c5ae95c
Oleg991 <o.n.eremenko@gmail.com>
2021-03-03 10:39:32 +0400

    Models added

---
+++ MoviesApp/Models/Cast.swift
@@ -0,0 +1,26 @@
+//
+//  Cast.swift
+//  MoviesApp
+//
+//  Created by Олег Еременко on 03.03.2021.
+//
+
+import Foundation
+
+struct CastResponse: Codable {
+    var cast: [Cast]
+}
+
+struct Cast: Codable, Identifiable {
+    var id: Int?
+    var name: String?
+    var character: String?
+    var profile_path: String?
+    var profilePhoto: String {
+        if let path = profile_path {
+            return "https://image.tmdb.org/t/p/original/\(path)"
+        } else {
+            return "https://picsum.photos/200/300"
+        }
+    }
+}
---
+++ MoviesApp/Models/Review.swift
@@ -0,0 +1,18 @@
+//
+//  Review.swift
+//  MoviesApp
+//
+//  Created by Олег Еременко on 03.03.2021.
+//
+
+import Foundation
+
+struct ReviewResponse: Codable {
+    var results: [Review]
+}
+
+struct Review: Codable, Identifiable {
+    var id: String?
+    var author: String?
+    var content: String?
+}
